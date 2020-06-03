76257364cfd5b555deba8ff9b4cf1cc9339d5dc0
nnhwin <6790548+nnhwin@users.noreply.github.com>
2020-06-03 11:02:26 +0630

    Update AccuracyForm.cs

--- YoutubeSpam/AccuracyForm.cs
+++ YoutubeSpam/AccuracyForm.cs
@@ -28,93 +28,15 @@ namespace SpamDetection
 
         public void StartAccuracy(ArrayList finalResult)
         {
-
-            //First Save in CSV File
-            var file = @"D:\FinalResult.csv";
-
-            using (var stream = File.CreateText(file))
-            {
-                for (int i = 0; i < finalResult.Count; i++)
-                {
-                    string first = finalResult[i].ToString();
-                    string csvRow = string.Format("{0}", first);
-                    stream.WriteLine(csvRow);
-                }
-                Console.WriteLine("Write.........CSV File");
-            }
-            this.dataGridView1.Refresh();
-            dataGridView1.DataSource = ReadCsv(@file);
-            al = finalResult;
         }
         public DataTable ReadCsv(string fileName)
         {
-            DataTable dt = new DataTable("Data");
-            using (OleDbConnection cn = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=\"" +
-                Path.GetDirectoryName(fileName) + "\";Extended Properties='text;HDR=yes;FMT=Delimited(,)';"))
-            {
-                //Execute select query
-                using (OleDbCommand cmd = new OleDbCommand(string.Format("select *from [{0}]", new FileInfo(fileName).Name), cn))
-                {
-                    cn.Open();
-                    using (OleDbDataAdapter adapter = new OleDbDataAdapter(cmd))
-                    {
-                        adapter.Fill(dt);
-                    }
-                }
-            }
-            return dt;
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            panel3.Visible = true;
-
-            //Calculate TF FN 
-            int TPCount = 0;
-            int TNCount = 0;
-            int FPCount = 0;
-            int FNCount = 0;
-            foreach (String str in al)
-            {
-
-                String[] arr = str.Split(',');
-                String num = arr[0].Trim();
-                String text = arr[1].Trim();
-                String label = arr[2].Trim();
-                String mylabel = arr[3].Trim();
-
-
-                if (label.Equals("1") || label.Equals("Spam"))
-                {
-                    if (mylabel.Equals("Spam"))
-                    {
-                        ++TPCount;
-                    }
-                    else
-                        ++FNCount;
-                }
-                else
-                {
-                    if (mylabel.Equals("Spam"))
-                        ++FNCount;
-                    else
-                        ++FPCount;
-                }
-
-                String output= "Confusion Matrix of Classification Result...................\n";
-                output += "True Positive=" + TPCount;
-                output += "\nTrue Negative=" + TNCount;
-                output += "\nFalse Positive=" + FPCount;
-                output += "\nFalse Negative=" + FNCount;
-
-                double accuracy =(double) (TPCount + TNCount) / (TPCount + TNCount + FPCount + FNCount);
-
-                output+="\nAccuracy = (TP+TN)/(TP+TN+FP+FN) ="+ "("+TPCount +"+"+ TNCount+") / ("+TPCount+" + "+TNCount+" + "+FPCount+" + "+FNCount+")\n\t\t="+accuracy+"\n";
-                output += "\n\nAccuracy value of Classification Result =" + accuracy * 100 + "%";
-
-                richTextBox1.Text = output;
-                Console.WriteLine(num + ".." + text + "......." + label + "...." + mylabel);
-            }
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
